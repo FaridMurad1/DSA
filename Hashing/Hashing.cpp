d6c28ba4de57ca21350708c1c6cbccd9da2ea01f
MD. MURAD HOSEN <136736998+FaridMurad1@users.noreply.github.com>
2024-05-25 00:32:38 +0600

    Update Hashing.cpp

--- Hashing/Hashing.cpp
+++ Hashing/Hashing.cpp
@@ -1,1 +1,39 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long
+using namespace std;
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll arr[n];
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    ll hash[13] = {0};
+    for (ll i = 0; i < n; i++)
+    {
+        hash[arr[i]]++;
+    }
+    ll test;
+    cin >> test;
+    while (test--)
+    {
+        ll number;
+        cin >> number;
+        cout << hash[number] << nl;
+    }
+}
+int main()
+{
+    int tc = 1;
+    // cin>>tc;
 
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        solve();
+    }
+    return 0;
+}
