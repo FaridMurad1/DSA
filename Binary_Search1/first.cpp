1525a777bae6f0d0e8bef3f038bc857db2125072
MD. MURAD HOSEN <136736998+FaridMurad1@users.noreply.github.com>
2024-05-25 00:36:30 +0600

    Create first.cpp

---
+++ Binary_Search1/first.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define nl '\n'
+#define ll long long
+using namespace std;
+void solve(){
+    
+}
+int main(){
+    int tc=1;    
+    // cin>>tc;
+    
+    for(int i=1;i<=tc;i++){
+        // cout<<"Case #"<<i<<": ";
+        solve();
+    }
+    return 0;
+}
